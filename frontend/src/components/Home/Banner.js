684b1918e4fbdbb2ddba79dae00de89d22e41c5c
Mike Vardy <mike.vardy@redspace.com>
2022-08-13 08:10:37 -0400

    add search ui

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,28 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = ({onSearchTitle}) => {
+  const handleChange = (ev) => {
+    ev.preventDefault();
+    const title = ev.target.value;
+    if (!title || title.length < 3) {
+      return
+    }
+    onSearchTitle(
+      title,
+      (page) => agent.Items.byTag(title, page),
+      agent.Items.byTitle(title)
+    );
+  };
+   
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input type="text" placeholder="What are you looking for?" onChange={handleChange} />
           <span> the cool stuff.</span>
         </div>
       </div>
