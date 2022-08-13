c4ccb11d7f0bbab5c168db6a24c3d38cce24b0c1
Mike Vardy <mike.vardy@redspace.com>
2022-08-13 08:19:10 -0400

    lint fix

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -2,12 +2,12 @@ import React from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
-const Banner = ({onSearchTitle}) => {
+const Banner = ({ onSearchTitle }) => {
   const handleChange = (ev) => {
     ev.preventDefault();
     const title = ev.target.value;
     if (!title || title.length < 3) {
-      return
+      return;
     }
     onSearchTitle(
       title,
@@ -15,14 +15,19 @@ const Banner = ({onSearchTitle}) => {
       agent.Items.byTitle(title)
     );
   };
-   
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
-          <input type="text" id='search-box' placeholder="What is it today that you truely desire?" onChange={handleChange} />
+          <input
+            type="text"
+            id="search-box"
+            placeholder="What is it today that you truely desire?"
+            onChange={handleChange}
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
